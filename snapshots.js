fd836ad409bf7f6f492d92114d165652dab96c03
lucent1090 <lucent1090@gmail.com>
2020-03-12 16:57:03 -0300

    fix bug

--- snapshots.js
+++ snapshots.js
@@ -1,6 +1,15 @@
+const contentful = require("contentful");
 const PercyScript = require('@percy/script');
+const _ = require('lodash');
 
 const websiteDEId = '7hTdOumEmWUmcuwi6ykKg2';
+
+const client = contentful.createClient({
+    space: "txhaodyqr481",
+    accessToken: "1ae7da438a372b631dd85dd0569282a2829f8e75f062bba38922f5e22158bd32",
+    environment: "master",
+    host: "cdn.contentful.com"
+  });
 
 async function getRoutes() {
     const response = await client.getEntries({
@@ -10,18 +19,24 @@ async function getRoutes() {
             'fields.websites.sys.id': websiteDEId
           });
           
-    return response.items;
+    return response.items.map(item => (item.fields.url));
 };
 
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(async (page, percySnapshot) => {
 
     const routes = await getRoutes();
-
-    routes.slice(0, 5).forEach(async route => {
-        await page.goto(route);
-        await percySnapshot(route);
+    const query = routes.slice(0, 3).map(route => {
+        return async route => {
+            await page.goto(route);
+            await percySnapshot(route);
+        };
     });
+    Promise.all(query)
+
+    // const url1 = routes[0].fields.url;
+    // await page.goto(url1);
+    // await percySnapshot(url1);
 //   await page.goto('http://google.com');
 //   await percySnapshot('Google home page');
 
