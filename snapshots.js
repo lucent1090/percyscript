ccc8a0ce4eb659a4ded055c0f6af5e098caebc99
lucent1090 <lucent1090@gmail.com>
2020-03-12 13:39:53 -0300

    init

---
+++ snapshots.js
@@ -0,0 +1,14 @@
+const PercyScript = require('@percy/script');
+
+// A script to navigate our app and take snapshots with Percy.
+PercyScript.run(async (page, percySnapshot) => {
+  await page.goto('http://google.com');
+  await percySnapshot('Google home page');
+
+  await page.type('.gLFyf', 'test input');
+  await percySnapshot('Google input test')
+  // Enter a new to-do.
+  // await page.type('.new-todo', 'A really important todo');
+  // await page.keyboard.press('Enter');
+  // await percySnapshot('TodoMVC with a new todo', { widths: [768, 992, 1200] });
+});
