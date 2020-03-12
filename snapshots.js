249b354b0703fa9e43887db032d935ace9f127d5
lucent1090 <lucent1090@gmail.com>
2020-03-12 16:44:16 -0300

    include contentful

--- snapshots.js
+++ snapshots.js
@@ -1,12 +1,32 @@
 const PercyScript = require('@percy/script');
 
+const websiteDEId = '7hTdOumEmWUmcuwi6ykKg2';
+
+async function getRoutes() {
+    const response = await client.getEntries({
+            content_type: "route",
+            include: 1,
+            locale: 'de-DE',
+            'fields.websites.sys.id': websiteDEId
+          });
+          
+    return response.items;
+};
+
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('http://google.com');
-  await percySnapshot('Google home page');
 
-  await page.type('.gLFyf', 'test input 111');
-  await percySnapshot('Google input test')
+    const routes = await getRoutes();
+
+    routes.slice(0, 5).forEach(route => {
+        await page.goto(route);
+        await percySnapshot(route);
+    });
+//   await page.goto('http://google.com');
+//   await percySnapshot('Google home page');
+
+//   await page.type('.gLFyf', 'test input 222');
+//   await percySnapshot('Google input test')
   // Enter a new to-do.
   // await page.type('.new-todo', 'A really important todo');
   // await page.keyboard.press('Enter');
